e7c669405b0e688541bcc06460898d6e05d0e71c
hamedsalim <hamediran1999@gmail.com>
2022-01-28 19:19:28 +0330

    read data from json file

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -2,10 +2,10 @@
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { RdsSnapshotExportPipelineStack, RdsEventId } from '../lib/rds-snapshot-export-pipeline-stack';
-
+let jsonData = require('../data.json');
 const app = new cdk.App();
 new RdsSnapshotExportPipelineStack(app, 'RdsSnapshotExportToS3Pipeline', {
-  dbName: '<existing-rds-database-name>',
+  dbName: jsonData.dbName,
   rdsEventId: RdsEventId.DB_AUTOMATED_SNAPSHOT_CREATED,
-  s3BucketName: '<desired-s3-bucket-name>',
+  s3BucketName: jsonData.s3BucketName,
 });
