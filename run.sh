dcc94ee3b6b3134ca718eaffda695cb4893e0bb4
hamedsalim <hamediran1999@gmail.com>
2022-01-28 19:20:06 +0330

    create json file from console imput and create cloudformation

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+echo "BucketName name is : "
+read BucketName
+echo "DBName name is : "
+read DBName
+
+echo '{"s3BucketName": "'"$BucketName"'", "dbName": "'"$DBName"'"}' | jq > data.json
+
+npm install
+npm run cdk bootstrap
+npm run cdk deploy
