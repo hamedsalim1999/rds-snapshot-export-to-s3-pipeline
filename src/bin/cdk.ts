85b146cba30419a9509f1d4481ec6c329d29ff97
hamedsalim <hamediran1999@gmail.com>
2022-02-01 17:57:00 +0330

    add data.json and fix path

--- src/bin/cdk.ts
+++ src/bin/cdk.ts
@@ -2,7 +2,7 @@
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { RdsSnapshotExportPipelineStack, RdsEventId } from '../lib/rds-snapshot-export-pipeline-stack';
-let jsonData = require('./data.json');
+let jsonData = require('../data.json');
 const app = new cdk.App();
 new RdsSnapshotExportPipelineStack(app, 'RdsSnapshotExportToS3Pipeline', {
   dbName: jsonData.dbName,
