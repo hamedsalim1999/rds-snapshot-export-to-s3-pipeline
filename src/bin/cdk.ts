502a451b3e0b5589e2f6ef00a7bf2e9f814b8983
hamedsalim <hamediran1999@gmail.com>
2022-02-01 17:54:31 +0330

    fix path

--- src/bin/cdk.ts
+++ src/bin/cdk.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { RdsSnapshotExportPipelineStack, RdsEventId } from './src/lib/rds-snapshot-export-pipeline-stack';
+import { RdsSnapshotExportPipelineStack, RdsEventId } from '../lib/rds-snapshot-export-pipeline-stack';
 let jsonData = require('./data.json');
 const app = new cdk.App();
 new RdsSnapshotExportPipelineStack(app, 'RdsSnapshotExportToS3Pipeline', {
